b7fbf45d35c9502cc9b36b7bbf137b0553bbcef9
Kayan Lira <kayan.lira@m4u.com.br>
2022-02-14 10:15:32 -0300

    unknow host kafka

--- src/main/kotlin/br/com/boasaude/msdocumentos/domain/usecase/SaveMedicalExamUseCase.kt
+++ src/main/kotlin/br/com/boasaude/msdocumentos/domain/usecase/SaveMedicalExamUseCase.kt
@@ -4,6 +4,9 @@ import br.com.boasaude.msdocumentos.application.dto.MedicalExamDTO
 import br.com.boasaude.msdocumentos.domain.model.ReferenceEntity
 import br.com.boasaude.msdocumentos.domain.repository.DocumentRepository
 import br.com.boasaude.msdocumentos.infrastructure.adapter.MsAssociadosApi
+import br.com.boasaude.msdocumentos.infrastructure.adapter.RestApi
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,10 +14,11 @@ class SaveMedicalExamUseCase(
     private val documentRepository: DocumentRepository,
     private val msAssociadosApi: MsAssociadosApi
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(SaveMedicalExamUseCase::class.java)
+
     fun execute(medicalExamDTO: MedicalExamDTO) {
         val document = medicalExamDTO.toDocument()
-        document.referenceId =
-            this.msAssociadosApi.getAssociateIdByDocumentNumber(medicalExamDTO.associate.documentNumber)
+        document.referenceId = this.msAssociadosApi.getAssociateIdByDocumentNumber(medicalExamDTO.associate.documentNumber)
         document.referenceEntity = ReferenceEntity.ASSOCIATE
         this.documentRepository.save(document)
     }
