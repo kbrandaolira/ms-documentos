eb7e18ab9c4354a7ce470cfb4571ac4ecf416988
Kayan Lira <kayan.lira@m4u.com.br>
2022-02-23 09:42:02 -0300

    bugfix

--- src/main/kotlin/br/com/boasaude/msdocumentos/application/handler/MedicalExamHandler.kt
+++ src/main/kotlin/br/com/boasaude/msdocumentos/application/handler/MedicalExamHandler.kt
@@ -24,7 +24,7 @@ class MedicalExamHandler(
     ): List<MedicalExamDTO> {
         val dtos = mutableListOf<MedicalExamDTO>()
         this.documentRepository.findByTypeAndReferenceEntityAndReferenceIdOrderByRealizationDateDesc(
-            DocumentType.valueOf(documentType),
+            documentType,
             referenceType,
             referenceId
         ).forEach {
--- src/main/kotlin/br/com/boasaude/msdocumentos/domain/usecase/SaveMedicalExamUseCase.kt
+++ src/main/kotlin/br/com/boasaude/msdocumentos/domain/usecase/SaveMedicalExamUseCase.kt
@@ -4,7 +4,6 @@ import br.com.boasaude.msdocumentos.application.dto.MedicalExamDTO
 import br.com.boasaude.msdocumentos.domain.model.ReferenceEntity
 import br.com.boasaude.msdocumentos.domain.repository.DocumentRepository
 import br.com.boasaude.msdocumentos.infrastructure.adapter.MsAssociadosApi
-import br.com.boasaude.msdocumentos.infrastructure.adapter.RestApi
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -18,7 +17,7 @@ class SaveMedicalExamUseCase(
 
     fun execute(medicalExamDTO: MedicalExamDTO) {
         val document = medicalExamDTO.toDocument()
-        document.referenceId = this.msAssociadosApi.getAssociateIdByDocumentNumber(medicalExamDTO.associate.documentNumber)
+        document.referenceId = this.msAssociadosApi.getAssociateIdByDocumentNumber(medicalExamDTO.associate!!.documentNumber)
         document.referenceEntity = ReferenceEntity.ASSOCIATE
         this.documentRepository.save(document)
     }
